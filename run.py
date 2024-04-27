86105ca2d73b0df1c3aac7cecb51c515a3115d00
mehdixlabetix <cherifmehdi706@gmail.com>
2024-04-27 02:31:02 +0100

    improve structure

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-from app import create_app
+from flask_socketio import SocketIO
+
+from app import app, init_routes
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    init_routes()
+    socketio = SocketIO(app)
+    socketio.run(app, debug=True,  allow_unsafe_werkzeug=True)
